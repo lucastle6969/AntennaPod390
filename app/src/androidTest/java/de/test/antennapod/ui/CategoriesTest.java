01a605e7672ce66c09e17bc9e08655df8eeff86f
Mstc08 <minjun_TC@hotmail.com>
2019-03-26 12:33:23 -0400

    [#139] Added a test file for categories UI test

---
+++ app/src/androidTest/java/de/test/antennapod/ui/CategoriesTest.java
@@ -0,0 +1,68 @@
+package de.test.antennapod.ui;
+
+import android.content.SharedPreferences;
+import android.test.ActivityInstrumentationTestCase2;
+
+import android.content.Context;
+import com.robotium.solo.Solo;
+
+import de.danoeh.antennapod.activity.MainActivity;
+import de.danoeh.antennapod.core.storage.PodDBAdapter;
+
+/**
+ * User interface tests for Categories Feature
+ */
+
+public class CategoriesTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private Solo solo;
+    private Context context;
+    private UITestUtils uiTestUtils;
+    private SharedPreferences prefs;
+
+    public CategoriesTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        this.context = getInstrumentation().getTargetContext();
+        uiTestUtils = new UITestUtils(context);
+        uiTestUtils.setup();
+
+        // create new database
+        PodDBAdapter.init(context);
+        PodDBAdapter.deleteDatabase();
+        PodDBAdapter adapter = PodDBAdapter.getInstance();
+        adapter.open();
+        adapter.close();
+
+        // override first launch preference
+        // do this BEFORE calling getActivity()!
+        prefs = getInstrumentation().getTargetContext().getSharedPreferences(MainActivity.PREF_NAME, Context.MODE_PRIVATE);
+        prefs.edit().putBoolean(MainActivity.PREF_IS_FIRST_LAUNCH, false).commit();
+
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        uiTestUtils.tearDown();
+        solo.finishOpenedActivities();
+
+        PodDBAdapter.deleteDatabase();
+
+        // reset preferences
+        prefs.edit().clear().commit();
+
+        super.tearDown();
+    }
+
+    private void openNavDrawer() {
+        solo.clickOnImageButton(0);
+        getInstrumentation().waitForIdleSync();
+    }
+
+
+}
