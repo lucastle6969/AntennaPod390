27083f8b1a7b768cc6ad6dd7e5852c6cf5edc9a8
Mstc08 <minjun_TC@hotmail.com>
2019-04-10 18:57:14 -0400

    [#173] Added UI test for adding a recommended radio station to own list, test for adding a new radio station and a test for playing a radio station
    Co-authored-by: Vineet <vineetkoppalkar@users.noreply.github.com>

---
+++ app/src/androidTest/java/de/test/antennapod/ui/RadioTest.java
@@ -0,0 +1,139 @@
+package de.test.antennapod.ui;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.ImageButton;
+
+import com.robotium.solo.Solo;
+
+import de.danoeh.antennapod.R;
+import de.danoeh.antennapod.activity.MainActivity;
+import de.danoeh.antennapod.core.feed.RadioStream;
+import de.danoeh.antennapod.core.storage.DBWriter;
+import de.danoeh.antennapod.core.storage.PodDBAdapter;
+
+import static java.lang.Thread.sleep;
+
+public class RadioTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private Solo solo;
+    private Context context;
+    private UITestUtils uiTestUtils;
+    private SharedPreferences prefs;
+
+    public RadioTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        this.context = getInstrumentation().getTargetContext();
+        uiTestUtils = new UITestUtils(context);
+        uiTestUtils.setup();
+
+        // create new database
+        PodDBAdapter.init(context);
+        PodDBAdapter.deleteDatabase();
+        PodDBAdapter adapter = PodDBAdapter.getInstance();
+        adapter.open();
+        insertRecommendedRadio();
+        adapter.close();
+
+        // override first launch preference
+        // do this BEFORE calling getActivity()!
+        prefs = getInstrumentation().getTargetContext().getSharedPreferences(MainActivity.PREF_NAME, Context.MODE_PRIVATE);
+        prefs.edit().putBoolean(MainActivity.PREF_IS_FIRST_LAUNCH, false).commit();
+
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        uiTestUtils.tearDown();
+        solo.finishOpenedActivities();
+
+        PodDBAdapter.deleteDatabase();
+
+        // reset preferences
+        prefs.edit().clear().commit();
+
+        super.tearDown();
+    }
+
+    private void insertRecommendedRadio() {
+        RadioStream uncategorizedCategory = new RadioStream(-1, "BBC media", "http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio2_mf_p");
+
+        synchronized (this) {
+            try {
+                DBWriter.setRecommendedRadioStreamTest(uncategorizedCategory);
+                sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void openNavDrawer() {
+        solo.clickOnImageButton(0);
+        getInstrumentation().waitForIdleSync();
+    }
+
+    private String getActionbarTitle() {
+        return ((MainActivity) solo.getCurrentActivity()).getSupportActionBar().getTitle().toString();
+    }
+
+    private void goingToRadioPage() throws Exception{
+        uiTestUtils.addLocalFeedData(true);
+        openNavDrawer();
+        solo.clickOnText(solo.getString(R.string.radio_stream_label));
+        solo.waitForView(android.R.id.list);
+    }
+
+    public void testAddingRecommendedRadioToOwnList() throws Exception{
+        goingToRadioPage();
+        solo.clickOnText(solo.getString(R.string.recommended_radio_streams));
+
+        ImageButton addRecommendedRadioButton = (ImageButton) solo.getView("imgAddToList");
+        solo.clickOnView(addRecommendedRadioButton);
+        solo.waitForDialogToOpen();
+        solo.clickOnText("Confirm");
+
+        solo.clickOnText(solo.getString(R.string.my_radio_streams));
+        assertFalse(solo.searchText(solo.getString(R.string.no_radio_stream_available), true));
+    }
+
+    public void testAddingNewRadioToList() throws Exception{
+        String URL = "http://stream.redbullradio.com/main";
+
+        goingToRadioPage();
+
+        solo.clickOnView(solo.getView(R.id.addRadioStreamBtn));
+        solo.waitForDialogToOpen();
+        solo.enterText(1, URL);
+        solo.clickOnText("Confirm");
+
+        solo.clickOnText(solo.getString(R.string.my_radio_streams));
+        assertTrue(solo.searchText("Radio Stream 1", true));
+    }
+
+    public void testPlayingRadio() throws Exception{
+        String URL = "http://stream.redbullradio.com/main";
+
+        goingToRadioPage();
+
+        solo.clickOnView(solo.getView(R.id.addRadioStreamBtn));
+        solo.waitForDialogToOpen();
+        solo.enterText(1, URL);
+        solo.clickOnText("Confirm");
+
+        ImageButton playRadioButton = (ImageButton) solo.getView("imgRadioStreamPlay");
+        solo.clickOnView(playRadioButton);
+        solo.sleep(5000);
+
+        solo.clickOnText(solo.getString(R.string.play_label));
+        assertTrue(solo.searchText(solo.getString(R.string.pause_label), true));
+    }
+
+}
