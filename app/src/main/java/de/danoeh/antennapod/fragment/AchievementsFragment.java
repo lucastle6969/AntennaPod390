05e797ffb265899dd412893f75e45064c9c06c43
Michael-Gagnon <m.gagnon04@hotmail.com>
2019-04-11 22:11:15 -0400

    [#185] Added new buttons in menu to allow reseting and toggling of achievements plus a confirmation window.

--- app/src/main/java/de/danoeh/antennapod/fragment/AchievementsFragment.java
+++ app/src/main/java/de/danoeh/antennapod/fragment/AchievementsFragment.java
@@ -1,15 +1,20 @@
 package de.danoeh.antennapod.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +24,8 @@ import de.danoeh.antennapod.R;
 import de.danoeh.antennapod.activity.MainActivity;
 import de.danoeh.antennapod.adapter.AchievementsAdapter;
 import de.danoeh.antennapod.core.achievements.Achievement;
-import de.danoeh.antennapod.core.storage.DBReader;
+import de.danoeh.antennapod.core.achievements.AchievementManager;
+import de.danoeh.antennapod.core.preferences.UserPreferences;
 
 public class AchievementsFragment extends Fragment {
 
@@ -32,7 +38,7 @@ public class AchievementsFragment extends Fragment {
     private ConcurrentHashMap<String, Achievement> achievementMap;
     private List<Achievement> achievementList;
     private TextView page_title_rank;
-    private ImageView total_rank_image;
+    private TextView total_rank_icon;
 
     public AchievementsFragment (){
 
@@ -47,7 +53,7 @@ public class AchievementsFragment extends Fragment {
         // but unless we say we do, old options menus sometimes
         // persist.  mfietz thinks this causes the ActionBar to be invalidated
         setHasOptionsMenu(true);
-        achievementMap = DBReader.getAchievements();
+        achievementMap = AchievementManager.getInstance(null).getAchievements();
         achievementList = toList(achievementMap);
     }
 
@@ -59,9 +65,12 @@ public class AchievementsFragment extends Fragment {
         achievementsAdapter = new AchievementsAdapter(achievementList, getActivity());
         achievementsAdapter.setContext(this.getActivity());
         page_title_rank = root.findViewById(R.id.achievement_page_title_rank);
-        total_rank_image = root.findViewById(R.id.achievement_rank_image);
-
-        page_title_rank.setText("Your Rank: Rookie" );
+        total_rank_icon = root.findViewById(R.id.achievement_total_rank_icon);
+        total_rank_icon.setBackgroundResource(de.danoeh.antennapod.core.R.drawable.ic_achievement_star_1);
+        Integer userRank = AchievementManager.getInstance(null).getTotalRank();
+        String playerTitle = AchievementManager.getInstance(null).getRankTitle(userRank);
+        total_rank_icon.setText(userRank.toString());
+        page_title_rank.setText("Your Rank: " + playerTitle );
 
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
@@ -89,9 +98,66 @@ public class AchievementsFragment extends Fragment {
     }
 
     @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        if (!isAdded()) {
+            return;
+        }
+        getActivity().getMenuInflater().inflate(R.menu.achievements_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(id) {
+            case R.id.enableAchievements:
+                UserPreferences.setAchievementsToggle(!UserPreferences.getAchievementsToggle());
+                if(UserPreferences.getAchievementsToggle()) {
+                    Toast.makeText(getActivity(), "Achievements Enabled",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getActivity(), "Achievements Disabled",
+                            Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.resetAchievements:
+                confirmResetDialog();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onDestroy(){
         super.onDestroy();
         root = null;
     }
 
+    public void refresh(){
+        getFragmentManager().beginTransaction().detach(this).attach(this).commit();
+    }
+
+    private void confirmResetDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+        builder
+        .setMessage("Are you sure you want to reset your achievements? All of your progress will be lost.")
+        .setPositiveButton("Yes",  new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                AchievementManager.getInstance(null).resetAchievements();
+                achievementsAdapter = new AchievementsAdapter(toList(AchievementManager.getInstance(null).getAchievements()), getActivity());
+                refresh();
+                achievementsAdapter.notifyDataSetChanged();
+            }
+        })
+        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog,int id) {
+            }
+        })
+        .show();
+    }
 }
